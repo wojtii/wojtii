1d67c4e9c23d9e32fb1084d25bc1ff9ef3a02b0f
wojtii <wojciechniedbala@pm.me>
2021-10-11 15:33:56 +0200

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -2,21 +2,25 @@
 # based on https://chodounsky.com/2017/01/20/automate-your-macos-development-machine-setup/
 
 # TODO ohmyzsh
-# TODO doom emacs setup
 # TODO iterm config
+# TODO vscode
+# TODO neovim
 
 REPO_DIR=~/wojtii
 DOTFILES_DIR=$REPO_DIR/dotfiles
 
 xcode-select --install
 
+# macos
+# TODO run macos.sh
+
 # dotfiles
 ln -s $DOTFILES_DIR/.gitconfig ~/.gitconfig
-ln -s $DOTFILES_DIR/.vimrc ~/.vimrc
 ln -s $DOTFILES_DIR/.zshrc ~/.zshrc
 
 mkdir -p ~/.config
 ln -s $DOTFILES_DIR/starship.toml ~/.config/starship.toml
+ln -s $DOTFILES_DIR/starship.toml ~/.config/init.vim
 
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
